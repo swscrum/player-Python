19c379666a725cd0cd05f42d3bdbec1d7dd026ca
structix <janek@schoff.it>
2024-05-17 00:38:29 +0200

    Change decide to return list

--- logic/strategy.py
+++ logic/strategy.py
@@ -1,7 +1,8 @@
+from typing import List
 from models.game_state import GameState
 from models.player_action import PlayerAction
 
 
-def decide(gameState: GameState) -> PlayerAction:
+def decide(gameState: GameState) -> List[PlayerAction]:
     # TODO: place your logic here
-    return PlayerAction(0, 0, 0)
+    return [PlayerAction(0, 0, 0)]
