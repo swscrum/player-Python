974875e369f44a0f0406fcbec43a9bac4591fc09
Xenoreaper <erroit00@hs-esslingen.de>
2024-05-17 21:30:03 +0200

    debug base_overflow

--- logic/x_functions.py
+++ logic/x_functions.py
@@ -5,10 +5,13 @@ from models.game_state import GameState
 
 def base_overflow(base: Base, gamestate: GameState):
     if base.population > gamestate.config.base_levels[base.level].max_population:
+        print("base: overflow")
         return 1 # population is too high, people are dying, overflow
     elif base.population == gamestate.config.base_levels[base.level].max_population:
+        print("base: max")
         return 0 # population is at it's limit spend now
     else:
+        print("base: good")
         return -1 # population is fine, underflow
 
 
