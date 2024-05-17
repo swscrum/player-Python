0949401204f9f724e40f6c249419bd76be2b7124
kr1pt0n05 <sandrolipinski@outlook.com>
2024-05-17 22:09:47 +0200

    isnefioneio

--- logic/strategy.py
+++ logic/strategy.py
@@ -6,7 +6,7 @@ import math
 def get_own_bases(all_existing_bases):
     own_bases = []
     for base in all_existing_bases:
-        if base.player == "04":
+        if base.player == 4:
             own_bases.append(base)
     return own_bases
 
@@ -30,8 +30,6 @@ def get_closest_base(own_base, all_existing_bases):
             shortest_base = base
 
     return shortest_base
-
-
 
 def decide(gameState: GameState) -> List[PlayerAction]:
     own_bases = get_own_bases(gameState.bases)
