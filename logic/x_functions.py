e4da2333d261ff4fcdd2a8e0a259ad918639bb33
Xenoreaper <erroit00@hs-esslingen.de>
2024-05-17 21:20:26 +0200

    base_overflow should now work

--- logic/x_functions.py
+++ logic/x_functions.py
@@ -1,14 +1,15 @@
 from models.base import Base
 from models.base_level import BaseLevel
+from models.game_state import GameState
 
 
-def base_overflow(base: Base):
-    if base.population > BaseLevel(base.level).max_population:
-        return 1  # population is too high, people are dying
-    elif base.population == BaseLevel(base.level).max_population:
-        return 0  # population is at it's limit spend now
+def base_overflow(base: Base, gamestate: GameState):
+    if base.population > gamestate.config.base_levels[base.level].max_population:
+        return 1 # population is too high, people are dying
+    elif base.population == gamestate.config.base_levels[base.level].max_population:
+        return 0 # population is at it's limit spend now
     else:
-        return -1  # population is fine
+        return -1 # population is fine
 
 
 if __name__ == '__main__':
