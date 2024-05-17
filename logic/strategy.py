2cec5189f50a46c3cce3cb42d2c8cbf45107dc4c
konrad_john@t-online.de <noLr^]h!%695Z1pB9I[N>
2024-05-17 20:26:31 +0200

    moved function getdistance to own functions file

--- logic/strategy.py
+++ logic/strategy.py
@@ -6,10 +6,6 @@ from x_functions import *
 from one_functions import *
 
 
-def getdistance(x1: int, y1: int, z1: int, x2: int, y2: int, z2: int) -> int:
-    return int(((x1-x2)**2 + (y1-y1)**2 + (z1-z2)**2) ** 0.5)
-
-
 def decide(gamestate: GameState) -> List[PlayerAction]:
     actions: List[PlayerAction] = []
     mybases: List[Base] = []
