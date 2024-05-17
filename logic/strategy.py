b9c51d62f8fd1f8d411eabf887f3249b1c6b44bc
konrad_john@t-online.de <noLr^]h!%695Z1pB9I[N>
2024-05-17 19:48:18 +0200

    test commit to dev branch

--- logic/strategy.py
+++ logic/strategy.py
@@ -7,6 +7,10 @@ def getdistance(x1: int, y1: int, z1: int, x2: int, y2: int, z2: int) -> int:
     return int(((x1-x2)**2 + (y1-y1)**2 + (z1-z2)**2) ** 0.5)
 
 
+def test(hallo='welt'):
+    return hallo[::-1]
+
+
 def decide(gameState: GameState) -> List[PlayerAction]:
     # TODO: place your logic here
     return [PlayerAction(0, 0, 0)]
