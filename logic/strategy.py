c417c6d2c1433be01ddfe4e9d3f00bfc14ffe7b0
MasterEagle23 <ad.eagle.email@gmail.com>
2024-05-17 20:09:43 +0200

    added basic logic

--- logic/strategy.py
+++ logic/strategy.py
@@ -7,6 +7,18 @@ def getdistance(x1: int, y1: int, z1: int, x2: int, y2: int, z2: int) -> int:
     return int(((x1-x2)**2 + (y1-y1)**2 + (z1-z2)**2) ** 0.5)
 
 
-def decide(gamestate: GameState) -> List[PlayerAction]:
+def decide(gameState: GameState) -> List[PlayerAction]:
+    actions = List[PlayerAction]
+    myBases = List[Base]
+    otherBases = List[Base]
+    for base in gameState.bases:
+        if base.player:
+            myBases.append(base)
+        else:
+            otherBases.append(base)
+    for base in myBases:
+        if base.population > gameState.config.base_levels[base.level].upgrade_cost:
+            actions.append(PlayerAction(base.uid, base.uid, gameState.config.base_levels[base.level].upgrade_cost))
+
     # TODO: place your logic here
-    return [PlayerAction(0, 0, 0)]
+    return [PlayerAction(0, 0, 0
