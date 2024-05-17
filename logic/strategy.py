9515bcb889a1918c43f51565165281ce8a492390
konrad_john@t-online.de <noLr^]h!%695Z1pB9I[N>
2024-05-17 20:00:42 +0200

    test

--- logic/strategy.py
+++ logic/strategy.py
@@ -7,6 +7,9 @@ def getdistance(x1: int, y1: int, z1: int, x2: int, y2: int, z2: int) -> int:
     return int(((x1-x2)**2 + (y1-y1)**2 + (z1-z2)**2) ** 0.5)
 
 
+# testing comit
+
+
 def decide(gamestate: GameState) -> List[PlayerAction]:
     # TODO: place your logic here
     return [PlayerAction(0, 0, 0)]
