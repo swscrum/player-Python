c1c747511fe8b4b654ff686bdedadc6abe89e5c2
kr1pt0n05 <sandrolipinski@outlook.com>
2024-05-17 22:05:30 +0200

    NEw Push

--- logic/strategy.py
+++ logic/strategy.py
@@ -3,15 +3,21 @@ from models.game_state import GameState
 from models.player_action import PlayerAction
 import math
 
-def get_own_bases(gameState):
+def get_own_bases(all_existing_bases):
     own_bases = []
-    for base in gameState.bases:
+    for base in all_existing_bases:
         if base.player == "04":
             own_bases.append(base)
     return own_bases
 
 
+def get_neutral_bases(all_existing_bases):
+    neutral_bases = []
+    for base in all_existing_bases:
+        if base.player == 0:
+            neutral_bases.append(base)
 
+    return neutral_bases
 
 def get_closest_base(own_base, all_existing_bases):
     shortest_base = all_existing_bases[0]
@@ -19,7 +25,7 @@ def get_closest_base(own_base, all_existing_bases):
 
     for base in all_existing_bases:
         current_distance = base.distance(base)
-        if current_distance < shortest_distance and str(base) != str(own_base):
+        if current_distance < shortest_distance:
             shortest_distance = current_distance
             shortest_base = base
 
@@ -28,10 +34,7 @@ def get_closest_base(own_base, all_existing_bases):
 
 
 def decide(gameState: GameState) -> List[PlayerAction]:
-    own_bases = get_own_bases(gameState)
-
-    print(gameState)
-
-    base = get_closest_base(own_bases[0], own_bases)
+    own_bases = get_own_bases(gameState.bases)
+    neutral_bases = get_neutral_bases(gameState.bases)
 
     return [PlayerAction(0, 0, 0)]
