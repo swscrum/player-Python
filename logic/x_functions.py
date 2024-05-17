37b7093207000c7c6e97fc448a6f8feb10e75918
Xenoreaper <erroit00@hs-esslingen.de>
2024-05-17 22:20:22 +0200

    removing upgrade_base

--- logic/x_functions.py
+++ logic/x_functions.py
@@ -7,11 +7,6 @@ def base_overflow(base: Base):
     return base.population - gamestate.config.base_levels[base.level].max_population 
 # returns difference in base population and max population
 # +val: base underflow, -val: base overflow
-    
-def upgrade_base(base: Base, upby: int):
-    if None == upby:
-        upby = base.population / 2
-    return PlayerAction(base.uid, base.uid, upby)
 
 
 if __name__ == '__main__':
