5d011745582305d2226c5bb60d77a2889a6e958b
MasterEagle23 <ad.eagle.email@gmail.com>
2024-05-17 23:41:04 +0200

    added stuff

--- logic/strategy.py
+++ logic/strategy.py
@@ -7,26 +7,46 @@ from logic.one_functions import *
 from logic.me_functions import *
 
 gamestate: GameState
-actions: List[PlayerAction] = []
-mybases: List[Base] = []
-otherbases: List[Base] = []
 
 
 def decide(game_state: GameState) -> List[PlayerAction]:
     global gamestate
-    global actions
-    global mybases
-    global otherbases
     gamestate = game_state
+    actions: List[PlayerAction] = []
 
+    mybases, otherbases = get_base_list(game_state)
+
+    otherbases = sort_bases_by_pop(otherbases)
+
+    my_inactive_bases = mybases
+
+    for base in my_inactive_bases:
+        if base.population > gamestate.config.base_levels[base.level].upgrade_cost:
+            actions.append(upgrade(base))
+            my_inactive_bases.pop(my_inactive_bases.index(base))
+    
+    for base in my_inactive_bases:
+        if base.population > get_max_population(base)/2:
+            for hostilebase in otherbases:
+                tmp_action:PlayerAction = attack(base, hostilebase)
+                if tmp_action.src >= 0 and tmp_action.dest >= 0 and tmp_action.amount > 0:
+                    actions.append(tmp_action)
+                    my_inactive_bases.pop(my_inactive_bases.index(base))
+                    break
+
+    for base in my_inactive_bases:
+        pass
+
+    return actions
+
+
+def get_base_list(game_state: GameState) -> tuple[List[Base], List[Base]]:
     for base in gamestate.bases:
+        mybases: List[Base] = []
+        otherbases: List[Base] = []
         if base.player:
             mybases.append(base)
         else:
             otherbases.append(base)
-
-    for base in mybases:
-        if base.population > gamestate.config.base_levels[base.level].upgrade_cost:
-            actions.append(upgrade_base(base))
-
-    return actions
+    
+    return mybases, otherbases
