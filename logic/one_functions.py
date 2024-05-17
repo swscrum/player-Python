b2a7794c7e63f184b10e92653639b66af04ca2c1
konrad_john@t-online.de <noLr^]h!%695Z1pB9I[N>
2024-05-17 21:23:48 +0200

    added comment

--- logic/one_functions.py
+++ logic/one_functions.py
@@ -8,7 +8,7 @@ def getdistance(pos1, pos2) -> int:
     elif type(pos1) == type(pos2) == type(tuple[int, int, int]):
         return int(((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2 + (pos1[2] - pos2[2]) ** 2) ** 0.5)
     else:
-        return -1
+        return -1  # TypeError
 
 
 if __name__ == '__main__':
