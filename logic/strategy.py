4b9f30235e6172cacc6d03d66e00eca809b45d49
kr1pt0n05 <sandrolipinski@outlook.com>
2024-11-15 20:47:14 +0100

    changed sth

--- logic/strategy.py
+++ logic/strategy.py
@@ -5,8 +5,6 @@ from models.base import Base
 from models.game_state import GameState
 from models.player_action import PlayerAction
 
-OWN_ID = 1
-
 def distance(x1, y1, z1, x2, y2, z2):
     d = math.sqrt(math.pow(x1-x2, 2) + math.pow(y1-y2, 2) + math.pow(z1-z2, 2))
     return math.floor(d)
@@ -15,7 +13,7 @@ def distance(x1, y1, z1, x2, y2, z2):
 def get_own_bases(bases_list):
     own_bases = []
     for base in bases_list:
-        if base.uid == OWN_ID:
+        if base.player == 1:
             own_bases.append(base)
     return own_bases
 
@@ -23,7 +21,6 @@ def decide(gameState: GameState) -> List[PlayerAction]:
     # TODO: place your logic here
 
     own_bases = get_own_bases(gameState.bases)
-    for base in own_bases:
-        print(base)
+    print(len(own_bases))
 
     return [PlayerAction(0, 0, 0)]
